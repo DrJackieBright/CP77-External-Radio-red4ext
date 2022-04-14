2ff3fa9f439e6f1fd81515b98187e7a588adbf28
DrJackieBright <brightestjackie@gmail.com>
2022-04-14 00:52:56 -0400

    Increment version number

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use red4ext_rs::prelude::*;
 define_plugin! {
     name: "CP77-External-Radio",
     author: "DrJackieBright",
-    version: 2:1:0,
+    version: 2:1:1,
     on_register: {
         register_function!("play", play);
         register_function!("pause", pause);
