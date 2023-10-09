63974043ec6b70d55682e3b89788af88ef7d8164
DrJackieBright <brightestjackie@gmail.com>
2023-10-09 12:30:30 -0400

    Switch to main repo red4ext-rs Preemptive version up

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use red4ext_rs::prelude::*;
 define_plugin! {
     name: "CP77-External-Radio",
     author: "DrJackieBright",
-    version: 2:1:10,
+    version: 2:1:11,
     on_register: {
         register_function!("play", play);
         register_function!("pause", pause);
